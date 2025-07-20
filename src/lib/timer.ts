3a343611448883c037604bd9347ed98a169641ee
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2025-07-20 09:32:32 +0700

    Add comments to Timer

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -5,11 +5,17 @@ type ScheduledAction = {
   action: () => void;
 };
 
+// TODO: Timer methods should return handles that can be used to cancel a timer.
+
+/**
+ * A simple timer to schedule actions for execution in the future.
+ */
 export class Timer {
   private _time: number = 0;
 
   private _heap: Heap<ScheduledAction>;
 
+  // The default timer that is used when static methods are called.
   private static _default: Timer = new Timer();
 
   constructor() {
@@ -18,6 +24,11 @@ export class Timer {
     );
   }
 
+  /**
+   * Call the update method every frame to ensure timers will be invoked at the
+   * scheduled time.
+   * @param dt
+   */
   update(dt: number) {
     this._time += dt;
 
@@ -29,18 +40,38 @@ export class Timer {
     }
   }
 
+  /**
+   * Call the default update method every frame to ensure actions will be
+   * invoked at the scheduled time.
+   * @param dt
+   */
   static update(dt: number) {
     this._default.update(dt);
   }
 
+  /**
+   * Invoke an action after a delay.
+   */
   after(delay: number, action: () => void) {
     this._heap.push({ scheduledAt: this._time + delay, action: action });
   }
 
+  /**
+   * Use the default timer to invoke an action after a delay.
+   * @param delay
+   * @param action
+   */
   static after(delay: number, action: () => void) {
     this._default.after(delay, action);
   }
 
+  /**
+   * Invoke an action every few seconds for some duration.
+   * @param delay The delay between invocations.
+   * @param duration
+   * @param action
+   * @param onFinish
+   */
   every(
     delay: number,
     duration: number,
@@ -59,6 +90,14 @@ export class Timer {
     }
   }
 
+  /**
+   * Use the default timer to invoke an action every few seconds for some
+   * duration.
+   * @param delay The delay between invocations.
+   * @param duration
+   * @param action
+   * @param onFinish
+   */
   static every(
     delay: number,
     duration: number,
@@ -68,12 +107,20 @@ export class Timer {
     this._default.every(delay, duration, action, onFinish);
   }
 
+  /**
+   * Remove all scheduled timers, preventing actions from being invoked in the
+   * future.
+   */
   removeAllTimers() {
     while (this._heap.size() > 0) {
       this._heap.pop();
     }
   }
 
+  /**
+   * Remove all scheduled timers from the default timer, preventing actions
+   * from being invoked in the future.
+   */
   static removeAllTimers() {
     this._default.removeAllTimers();
   }
